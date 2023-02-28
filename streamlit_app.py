7ac8e7c9174f04a88e0d2f616d49431ada775116
Joel Latino <latinojoel@users.noreply.github.com>
2023-02-28 15:29:38 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.test("Omega 3 & Blueberry Oatmeal")
+streamlit.test("Kale, Spinach & Rocket Smoothie")
+streamlit.test("Hard-Boiled Free-Range Egg")
